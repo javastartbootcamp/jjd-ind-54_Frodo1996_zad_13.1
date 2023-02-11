29b850180b7e7020f4cbf652e965535635940c07
Krzysiek Przystarz <frodson35@gmail.com>
2023-02-11 10:59:59 +0100

    Brak dalszych pomysłów na rozwiązanie zadania... :(

--- src/main/java/pl/javastart/voting/Vote.java
+++ src/main/java/pl/javastart/voting/Vote.java
@@ -17,5 +17,6 @@ public class Vote {
 
     public Boolean getVote() {
         return vote;
+
     }
 }
--- src/main/java/pl/javastart/voting/VotingResult.java
+++ src/main/java/pl/javastart/voting/VotingResult.java
@@ -1,9 +1,55 @@
 package pl.javastart.voting;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 /**
  * Możesz dodać kolejne metody i pola do klasy. Nie zmieniaj istniejących metod.
  */
 public class VotingResult {
+    private static double voteFor;
+    private static double voteAgainst;
+    private static double voteAbstention;
+    private String voteOption;
+
+    public VotingResult(String voteOption) {
+        this.voteOption = voteOption;
+    }
+
+    public String getVoteOption() {
+        return voteOption;
+    }
+
+    public void setVoteOption(String voteOption) {
+        this.voteOption = voteOption;
+    }
+
+    public static double getVoteFor() {
+        return voteFor;
+    }
+
+    public static double getVoteAgainst() {
+        return voteAgainst;
+    }
+
+    public static double getVoteAbstention() {
+        return voteAbstention;
+    }
+
+    static void setVoteFor(double voteFor) {
+        voteFor++;
+        VotingResult.voteFor = voteFor;
+    }
+
+    static void setVoteAgainst(double voteAgainst) {
+        voteAgainst++;
+        VotingResult.voteAgainst = voteAgainst;
+    }
+
+    static void setVoteAbstention(double voteAbstention) {
+        voteAbstention++;
+        VotingResult.voteAbstention = voteAbstention;
+    }
 
     /**
      * Metoda powinna drukować wyniki głosowania w takiej postaci:
@@ -12,6 +58,9 @@ public class VotingResult {
      * Wstrzymało się: 8.47%
      */
     public void printResults() {
+        System.out.println("Głosów za: " + percentOfVotesFor() + "%");
+        System.out.println("Głosów przeciw: " + percentOfVotesAgainst() + "%");
+        System.out.println("Wstrzymało się: " + percentOfVotesAbstention() + "%");
         // metoda powinna drukować wyniki głosowania
     }
 
@@ -22,6 +71,35 @@ public class VotingResult {
      * Nie zmieniaj sygnatury tej metody!
      */
     public void printVoteForVoter(String voterName) {
+        if (voteOption.equals("z")) {
+            System.out.println(voterName + ": ZA");
+        } else if (voteOption.equals("p")) {
+            System.out.println(voterName + ": PRZECIW");
+        } else if (voteOption.equals("w")) {
+            System.out.println(voterName + ": WSTRZYMAŁ SIĘ");
+        } else {
+            System.err.println("Błędna wartość.");
+        }
+    }
 
+    public BigDecimal percentOfVotesFor() {
+        double allVotes = voteFor + voteAgainst + voteAbstention;
+        double percent = (voteFor / allVotes) * 100;
+        BigDecimal bigDecimal = new BigDecimal(percent);
+        return bigDecimal.setScale(2, RoundingMode.HALF_EVEN);
+    }
+
+    public BigDecimal percentOfVotesAgainst() {
+        double allVotes = voteFor + voteAgainst + voteAbstention;
+        double percent = (voteAgainst / allVotes) * 100;
+        BigDecimal bigDecimal = new BigDecimal(percent);
+        return bigDecimal.setScale(2, RoundingMode.HALF_EVEN);
+    }
+
+    public BigDecimal percentOfVotesAbstention() {
+        double allVotes = voteFor + voteAgainst + voteAbstention;
+        double percent = (voteAbstention / allVotes) * 100;
+        BigDecimal bigDecimal = new BigDecimal(percent);
+        return bigDecimal.setScale(2, RoundingMode.HALF_EVEN);
     }
 }
